5d18e50534338fa1d4ca579a55333f7dc8f7e873
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-06-23 20:47:58 -0300

    Customiza fontes

---
+++ front-end/src/assets/css/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Lato', sans-serif;
+  }
+  a {
+    color: #00FFFF;
+    text-decoration: none;
+  }
+`
