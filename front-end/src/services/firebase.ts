189a68a0a952d60e737369a19959f79d2291ad51
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-06-30 21:42:09 -0300

    Instala firebase

---
+++ front-end/src/services/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbHaa8cbUELq8OMPTjhAxC-8qUGuO_wiA",
+  authDomain: "pet-home-7e8cd.firebaseapp.com",
+  projectId: "pet-home-7e8cd",
+  storageBucket: "pet-home-7e8cd.appspot.com",
+  messagingSenderId: "744265294180",
+  appId: "1:744265294180:web:4f7273b05c3326f4e81a71"
+};
+
+
+const app = initializeApp(firebaseConfig);
