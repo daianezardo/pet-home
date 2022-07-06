8627c118bef8d44f95226454cc9c831326ba0b28
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-07-05 23:18:44 -0300

    Implementa tela de login

--- front-end/src/Routes.tsx
+++ front-end/src/Routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes as RDRoutes } from "react-router-dom";
 import { PublicOnlyRoute } from "./components/PublicOnlyRoute";
 import { HomeView } from "./Views/Home";
+import { LoginView } from "./Views/Home/Login";
 import { NewOrderView } from "./Views/NewOrder";
 import { NotFoundView } from "./Views/NotFoundView";
 import { RegisterView } from "./Views/Register";
@@ -12,7 +13,13 @@ export function Routes () {
             <Route path="/cadastro" element={
             <PublicOnlyRoute>
             <RegisterView />
-            </PublicOnlyRoute> }/>
+            </PublicOnlyRoute> }
+            />
+            <Route path="/login" element={
+            <PublicOnlyRoute>
+            <LoginView />
+            </PublicOnlyRoute> }
+            />
             <Route path="/novo-pedido" element={<NewOrderView />} />
             <Route path="*" element={<NotFoundView />} />
         </RDRoutes>
---
+++ front-end/src/Views/Home/Login/index.tsx
@@ -0,0 +1,83 @@
+import { FirebaseError } from "firebase/app";
+import { AuthErrorCodes } from "firebase/auth";
+import { useFormik } from "formik";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { CustomButton } from "../../../components/CustomButton";
+import { FormField } from "../../../components/FormField";
+import { Layout } from "../../../components/Layout";
+import { PageTitle } from "../../../components/PageTitle";
+import * as yup from 'yup';
+
+type FormValues = {
+    email: string
+    password: string
+  }
+
+export function LoginView () {
+    const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: yup.object().shape({
+      email: yup.string()
+        .required('Preencha o e-mail.')
+        .email('Preencha um e-mail válido.'),
+      password: yup.string()
+        .required('Preencha a senha.')
+    }),
+    onSubmit: async (values) => {
+      
+    }
+  })
+  const getFieldProps = (fieldName: keyof FormValues) => {
+    return {
+      ...formik.getFieldProps(fieldName),
+      controlId: `input-${fieldName}`,
+      error: formik.errors[fieldName],
+      isInvalid: formik.touched[fieldName] && !!formik.errors[fieldName],
+      isValid: formik.touched[fieldName] && !formik.errors[fieldName]
+    }
+  }
+    return (
+        <Layout>
+      <Container>
+        <Row className="justify-content-center">
+          <Col lg={4}>
+            <PageTitle>Login</PageTitle>
+            <Form onSubmit={formik.handleSubmit}>
+              <FormField
+                {...getFieldProps('email')}
+                label='E-mail'
+                type='email'
+                placeholder='Informe o seu e-mail de acesso'
+              />
+              <FormField
+                {...getFieldProps('password')}
+                label='Senha'
+                type='password'
+                placeholder='Informe sua senha de acesso'
+              />
+              <div className="d-grid mb-4">
+                <CustomButton
+                  type="submit"
+                  loading={formik.isValidating || formik.isSubmitting}
+                  disabled={formik.isValidating || formik.isSubmitting}
+                >
+                  Entrar
+                </CustomButton>
+              </div>
+              <p className="text-center">Não possui conta?<br/><Link to='/cadastro'>Cadastrar</Link></p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  )
+}
